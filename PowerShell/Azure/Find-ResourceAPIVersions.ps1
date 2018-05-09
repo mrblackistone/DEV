11bec08041008fb830d7300226589733cd8516a7
Michael Blackistone <miblacki@microsoft.com>
2018-05-09 16:10:59 -0400

    Initial

---
+++ PowerShell/Azure/Find-ResourceAPIVersions.ps1
@@ -0,0 +1,17 @@
+#The purpose of this script is to go through all of the resources you have access to across all of your Azure environment's subscriptions, 
+#and provide the two most recent available API versions for their respective provider namespaces and resource types.
+
+Get-azurermsubscription | ForEach-Object {
+    Select-AzureRmSubscription -Subscription $_.SubscriptionId
+    Get-AzureRmResource | ForEach-Object {
+        [string]$resourceType = $_.ResourceType
+        if ($resourceType -notin $a) {$a += $resourceType}
+    }
+}
+
+$a | ForEach-Object {
+    $providerNamespace = $_.split("/",2)[0]
+    $resourceTypeName = $_.split("/",2)[1]
+    Write-Host $_":";
+    ((Get-AzureRMResourceProvider -ProviderNamespace $providerNamespace).ResourceTypes | Where-Object ResourceTypeName -eq $resourceTypeName).ApiVersions[0,1];"";
+}
