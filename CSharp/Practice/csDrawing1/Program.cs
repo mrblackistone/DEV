ea4422b4356689259cd6c78cfe028dc5d04b9a40
M B <m.blackistone@outlook.com>
2019-01-29 00:26:31 -0500

    CS forms?

---
+++ CSharp/Practice/csDrawing1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace csDrawing1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Form f = new Form();
+            f.BackColor = Color.White;
+            f.FormBorderStyle = FormBorderStyle.None;
+            f.Bounds = Screen.PrimaryScreen.Bounds;
+            f.TopMost = true;
+
+            Application.EnableVisualStyles();
+            Application.Run(f);
+        }
+    }
+}
