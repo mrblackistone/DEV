65ef5c4d4d8ff9742e7d328fe50425c8361eb04a
M B <m.blackistone@outlook.com>
2019-01-29 18:50:39 -0500

    More CSharp plus new Python intro stuff

---
+++ Python/Practice/pyBasics.py
@@ -0,0 +1,35 @@
+"This is a string"
+6 + 5
+print("Hello, world!")
+print('Single quotes work, too.')
+print('Inside of single quotes, you "can" have double quotes.')
+print("Inside of double quotes, you 'can' have single quotes.")
+2 * (3 + 4)
+8 / 4
+print("Division of integers yields floats, even if evenly divided")
+-7 + 8
+print("Negative numbers are fine.")
+5 + 6.0
+print("Any calculation containing a float becomes a float, even if whole")
+8**2
+print("Double asterisk is exponential.")
+8//2
+print("Double slash is floor division (where remainder is dropped).")
+7%2
+print("Percent sign is modulus.")
+print("Backslash escapes itself \\ and newline \n and double quotes in double quotes \"")
+print("""Triple double quotes
+allow you to use enter to get new lines.""")
+print(1+1)
+print(2*"string")
+input("Enter something: ")
+print("Input adds escape charaters to the string as needed.")
+"spam"+'eggs'
+print("concat"+"enation")
+"2" + "2"
+int("2") + int("3")
+4 * '2'
+float(input("Enter a number: ")) + float(input("Enter another number: "))
+x = 7
+print(x)
+print(x + 3)
