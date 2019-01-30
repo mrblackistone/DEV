7e3567728f163308d053a4ed13f546736887694a
Michael Blackistone <miblacki@microsoft.com>
2019-01-30 11:55:50 -0500

    Python introduction updates

--- Python/Practice/pyBasics.py
+++ Python/Practice/pyBasics.py
@@ -1,35 +1,55 @@
+#Getting help:  Help method calls help interpreter, and the keywords below list more info
+help()
+modules
+symbols
+keywords
+topics
+
+#Strings
 "This is a string"
-6 + 5
 print("Hello, world!")
 print('Single quotes work, too.')
 print('Inside of single quotes, you "can" have double quotes.')
 print("Inside of double quotes, you 'can' have single quotes.")
-2 * (3 + 4)
-8 / 4
-print("Division of integers yields floats, even if evenly divided")
--7 + 8
-print("Negative numbers are fine.")
-5 + 6.0
-print("Any calculation containing a float becomes a float, even if whole")
-8**2
-print("Double asterisk is exponential.")
-8//2
-print("Double slash is floor division (where remainder is dropped).")
-7%2
-print("Percent sign is modulus.")
 print("Backslash escapes itself \\ and newline \n and double quotes in double quotes \"")
 print("""Triple double quotes
-allow you to use enter to get new lines.""")
-print(1+1)
-print(2*"string")
-input("Enter something: ")
-print("Input adds escape charaters to the string as needed.")
-"spam"+'eggs'
-print("concat"+"enation")
-"2" + "2"
-int("2") + int("3")
-4 * '2'
-float(input("Enter a number: ")) + float(input("Enter another number: "))
-x = 7
+allow you to use enter key to break across new lines in a string.""")
+"spam"+'eggs' #Single and double quotes work identically
+
+#Numbers
+6 + 5 #Int plus int is int
+2 * (3 + 4) #Ints without division yield ints
+8 / 4 #division yields float
+-7 + 8 #Negatives are naturally defined
+5 + 6.0 #Any equation with a float will yield a float
+8**2 #Exponent
+8//2 #Floor division, remainder is dropped, outputs int
+7%2 #Modulus
+
+#Print outputs
+print(1+1) #Integer
+print(2*"string") #Clone string and concat the clones
+print("concat"+"enation") #Concatenation
+
+#Inputs
+input("Enter something: ") #Input adds escape charaters to the string as needed (\ converted to \\).
+
+#String/number combinations
+"2" + "2" #Outputs 22 as these are strings
+int("2") + int("3") #outputs 5 as integer method converts strings to ints
+4 * '2' #Outputs 2222 as 2 is a string
+float(input("Enter a number: ")) + float(input("Enter another number: ")) #Outputs float sum of inputs
+
+#Variables:  Names can be alphanumeric and underscores, but can't start with a number
+#Variables don't have types in Python, so different data types can be assigned to the same variable
+x = 7 #Define and assign variable
 print(x)
 print(x + 3)
+del x #Delete variable
+
+#In-place operators
+x += 2
+x -= 2
+x *= 2
+x %= 2
+x /= 2
---
+++ Python/Practice/pyControlStructures.py
@@ -0,0 +1,139 @@
+#Boolean and comparison operators
+trueVar = True
+falseVar = False
+trueVar == falseVar #Returns false
+trueVar != falseVar #Returns true
+8.0 > 5 #Returns true
+1 <= 5 #Returns true
+8.7 <= 8.70 #Returns true
+
+#If statements
+#Code blocks require indentation
+if 10 > 9 :
+    print("True 'nuf")
+print("Done")
+num = 12
+if num > 5 :
+    print("Bigger than 5")
+    if num >= 10 :
+        print("At least 10")
+
+#Else statements
+x = 4
+if x == 5 :
+    print("It's 5")
+else :
+    print("It's not 5")
+
+#Elif (ElseIf) statements
+num = 7
+if num == 5 :
+    print("It's 5")
+elif num == 7 :
+    print("It's 7")
+else :
+    print("Nope")
+
+#Boolean logic
+1 == 1 and 2 == 2 #Returns true
+1 == 1 and 2 == 3 #Returns false
+1 == 1 or 2 == 3 #Returns true
+not 1 == 1 #Returns false
+not 1 > 7 #Returns true
+#"if not true" returns false, and elif and else only evaluate if initial "if" is false, therefore...
+if not True :
+    print("1")
+elif not (1+1 == 3) :
+    print("2")
+else:
+    print("3")
+#...returns 2
+
+#Operator precedence:  Follows PEMDAS; comparison operators are higher precedence than boolean logic...
+False == False or True #Returns True
+False == (False or True) #Returns False
+(False == False) or True #Returns True
+#Precedence:
+# **            (Exponentiation)
+# ~, +, -       (Complement, unary plus and minus.  Method names for last two are +@ and -@)
+# *, /, %, //   (Multiply, divide, modulus, and floor division)
+# +, -          (Addition, subtraction)
+# >>, <<        (Right and left bitwise shift)
+# &             (Bitwise AND)
+# ^             (Bitwise XOR)
+# |             (Bitwise OR)
+# in, not in, is, is not, <, <=, >, >=, !=, ==
+# not
+# and
+# or
+# =, %=, /=, //=, -=, +=, *=, **=
+
+#While loops
+i = 1
+while i <= 5 :
+    print(i)
+    i += 1
+print("Done")
+while 1 == 1 : #Infinite loop
+    print("Looping")
+#Break stops a loop immediately
+i = 0
+while 1 == 1 : #or you could use "while True" for an infinite loop
+    print(i)
+    i += 1
+    if i >= 5 :
+        print("Breaking")
+        break
+print("Done")
+#Continue breaks only the current iteration of the loop, skipping back to the top
+
+#Lists (like Arrays)
+listVar = ["foo","bar","!!!"]
+print(listVar[0]) #Returns foo
+blankListVar = []
+singleItem = [5,] #Ending comma does not define an additional item, and can be left off if desired
+variedList = ["string",42,["foo","bar",],7.62]
+print(variedList[2][1]) #Returns bar
+stringVar = "Some words" #String is just a list of CHARs
+print(stringVar[2]) #Returns m
+variedList[0] = "foo" #Change value as list index location.  Can't do this for string, though
+print(variedList) #Returns with modified value on 0 index
+numsList = [1,2,3,4,5]
+print(numsList + [4,5,6]) #Returns longer concatenated single list
+print(numsList * 3) #Returns single list with contents in triplicate
+#List Operations
+wordList = ["foo","bar","desk","chair"]
+print("foo" in wordList) #Returns True
+print("jifweojfewoeo" in wordList) #Returns Falsee
+print(not "foo" in wordList) #Returns False
+print("foo" not in wordList) #Returns False, just with different order
+print("ifjweojfewo" not in wordList) #Returns True
+wordList.append("appended word") #Append method appends indexed item to list
+len(wordList) #Len function returns number of indexed items in list
+wordList.insert(2,"inserted word") #Insert method inserts a new value into a list, and shifts existing items to the right
+wordList.index("inserted word") #Index method finds the FIRST matching value and returns its index
+max(wordList) #Max function returns list item with greatest value.  Won't work with mixed str and num data types.
+min(wordList) #Min function returns list item with smallest value.  Won't work with mixed str and num data types.
+wordList.count("foo") #Count method returns number of instances of an object in a list
+wordList.remove() #Remove method removes the first matching object from a list
+wordList.reverse() #Reverse method reverses the order of the list object itself (doesn't just return a reversed order)
+#Range object.
+#Single argument ranges from 0 to int 1 below provided value.
+rangeList = list(range(10)) #Creates a list from a range object.  Without the call to "list" a range object is created instead.
+rangeObject = range(10) #Range object created.  This is NOT a list.
+rangeList #Returns: [0,1,2,3,4,5,6,7,8,9]
+rangeObject #Returns object: range(0,10)
+#  Two arguments range from first arg to 1 below second arg.
+z = list(range(3,8)) #Returns
+print(z) #Returns [3,4,5,6,7]
+# Three arguments are same as two, but third is interval.
+z = list(range(5,20,3))
+print(z) #Returns [5,8,11,14,17]
+
+#For loops
+
+
+
+
+
+
