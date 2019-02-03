90eb2ef18c00afcb61c56070944f72b24a92b7c8
M B <m.blackistone@outlook.com>
2019-02-03 14:29:12 -0800

    updates

---
+++ CSharp/Practice/Cards/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cards
+{
+    class Program
+    {
+        class Card {
+            public string Name { get; set; }
+            public string Suit { get; set; }
+            public int Value { get; set; }
+            public Card(string nm, string st, int vl) {
+                Name = nm;
+                Suit = st;
+                Value = vl;
+            }
+        }
+        //Main--------------------------------------------------------
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
