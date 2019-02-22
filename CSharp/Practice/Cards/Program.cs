f2cdf3c0a79c17d9a1bf38c800a959c1bd475b2e
M B <m.blackistone@outlook.com>
2019-02-22 15:50:48 -0500

    Trying to fix System namespacing missing issues for Shuffle folder

--- CSharp/Practice/Cards/Program.cs
+++ CSharp/Practice/Cards/Program.cs
@@ -3,28 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Cards
-{
-    class Program
-    {
-        class Card
-        {
+namespace Cards {
+    class Program {
+        class Card {
             public string Name { get; set; }
             public string Suit { get; set; }
             public int[] Value { get; set; }
-            public Card(string nm, string st, int[] vl)
-            {
+            public Card (string nm, string st, int[] vl) {
                 Name = nm;
                 Suit = st;
                 Value = vl;
             }
         }
-        public static List<T> Shuffle<T>(List<T> list)
-        {
-            Random rnd = new Random();
-            for (int i = 0; i < list.Count; i++)
-            {
-                int k = rnd.Next(0, i);
+        public static List<T> Shuffle<T> (List<T> list) {
+            Random rnd = new Random ();
+            for (int i = 0; i < list.Count; i++) {
+                int k = rnd.Next (0, i);
                 T value = list[k];
                 list[k] = list[i];
                 list[i] = value;
@@ -33,31 +27,26 @@ namespace Cards
         }
 
         //Main--------------------------------------------------------
-        static void Main(string[] args)
-        {
+        static void Main (string[] args) {
             int numDecks = 0;
-            Console.Write("Enter number of decks: ");
-            numDecks = Convert.ToInt32(Console.ReadLine());
+            Console.Write ("Enter number of decks: ");
+            numDecks = Convert.ToInt32 (Console.ReadLine ());
             string[] suitsArray = { "Hearts", "Spades", "Diamonds", "Clubs" };
             string[] namesArray = { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
             int[] currVals = new int[2];
-            List<Card> fullDeckList = new List<Card>();
+            List<Card> fullDeckList = new List<Card> ();
             int convertedName;
             int cardCountVar = 0;
 
-            for (int a = 1; a <= numDecks; a++)
-            {
-                foreach (string suit in suitsArray)
-                {
-                    foreach (string name in namesArray)
-                    {
-                        switch (name)
-                        {
+            for (int a = 1; a <= numDecks; a++) {
+                foreach (string suit in suitsArray) {
+                    foreach (string name in namesArray) {
+                        switch (name) {
                             case "Ace":
-                                Array.Resize(ref currVals, 2);
+                                Array.Resize (ref currVals, 2);
                                 currVals[0] = 1;
                                 currVals[1] = 13;
-                                fullDeckList.Add(new Card(name, suit, (int[])currVals.Clone()));
+                                fullDeckList.Add (new Card (name, suit, (int[]) currVals.Clone ()));
                                 //Clone required, otherwise object will reference variable rather than its value.
                                 //Name and Suit don't need to be cloned as they are not objects, and value is assumed.
                                 cardCountVar++;
@@ -66,16 +55,16 @@ namespace Cards
                             case "Jack":
                             case "Queen":
                             case "King":
-                                Array.Resize(ref currVals, 1);
+                                Array.Resize (ref currVals, 1);
                                 currVals[0] = 10;
-                                fullDeckList.Add(new Card(name, suit, (int[])currVals.Clone()));
+                                fullDeckList.Add (new Card (name, suit, (int[]) currVals.Clone ()));
                                 cardCountVar++;
                                 break;
                             default:
-                                convertedName = Convert.ToInt32(name);
-                                Array.Resize(ref currVals, 1);
+                                convertedName = Convert.ToInt32 (name);
+                                Array.Resize (ref currVals, 1);
                                 currVals[0] = convertedName;
-                                fullDeckList.Add(new Card(name, suit, (int[])currVals.Clone()));
+                                fullDeckList.Add (new Card (name, suit, (int[]) currVals.Clone ()));
                                 cardCountVar++;
                                 break;
                         }
@@ -84,33 +73,33 @@ namespace Cards
             }
 
             //Display output
-            Console.WriteLine("\nPre-Shuffle:");
-            Console.WriteLine("Name: {0}; Suit: {1}; Values: {2} {3}", fullDeckList[0].Name, fullDeckList[0].Suit, fullDeckList[0].Value[0], fullDeckList[0].Value[1]);
-            Console.WriteLine("Name: {0}; Suit: {1}; Values: {2}", fullDeckList[8].Name, fullDeckList[8].Suit, fullDeckList[8].Value[0]);
-            Console.WriteLine("Name: {0}; Suit: {1}; Values: {2}", fullDeckList[10].Name, fullDeckList[10].Suit, fullDeckList[10].Value[0]);
+            Console.WriteLine ("\nPre-Shuffle:");
+            Console.WriteLine ("Name: {0}; Suit: {1}; Values: {2} {3}", fullDeckList[0].Name, fullDeckList[0].Suit, fullDeckList[0].Value[0], fullDeckList[0].Value[1]);
+            Console.WriteLine ("Name: {0}; Suit: {1}; Values: {2}", fullDeckList[8].Name, fullDeckList[8].Suit, fullDeckList[8].Value[0]);
+            Console.WriteLine ("Name: {0}; Suit: {1}; Values: {2}", fullDeckList[10].Name, fullDeckList[10].Suit, fullDeckList[10].Value[0]);
 
             //Shuffle deck
-            fullDeckList = Shuffle(fullDeckList);
+            fullDeckList = Shuffle (fullDeckList);
 
             //Display shuffled results from same positions
-            Console.WriteLine("\nPost-Shuffle:");
-            Console.WriteLine("Name: {0}; Suit: {1}; Values: {2}", fullDeckList[0].Name, fullDeckList[0].Suit, fullDeckList[0].Value[0]);
-            Console.WriteLine("Name: {0}; Suit: {1}; Values: {2}", fullDeckList[8].Name, fullDeckList[8].Suit, fullDeckList[8].Value[0]);
-            Console.WriteLine("Name: {0}; Suit: {1}; Values: {2}\n", fullDeckList[10].Name, fullDeckList[10].Suit, fullDeckList[10].Value[0]);
+            Console.WriteLine ("\nPost-Shuffle:");
+            Console.WriteLine ("Name: {0}; Suit: {1}; Values: {2}", fullDeckList[0].Name, fullDeckList[0].Suit, fullDeckList[0].Value[0]);
+            Console.WriteLine ("Name: {0}; Suit: {1}; Values: {2}", fullDeckList[8].Name, fullDeckList[8].Suit, fullDeckList[8].Value[0]);
+            Console.WriteLine ("Name: {0}; Suit: {1}; Values: {2}\n", fullDeckList[10].Name, fullDeckList[10].Suit, fullDeckList[10].Value[0]);
 
             //Display full shuffled deck
-            int x,y;
-            string w,z;
-            for ( int a = 0; a < cardCountVar; a++ ) {
+            int x, y;
+            string w, z;
+            for (int a = 0; a < cardCountVar; a++) {
                 w = fullDeckList[a].Name;
                 z = fullDeckList[a].Suit;
-                if ( fullDeckList[0].Value.Length == 2 ) { //Trying to get this to work.  Aces not appearing.
+                if (fullDeckList[0].Value.Length == 2) { //Trying to get this to work.  Aces not appearing.
                     x = fullDeckList[a].Value[0];
                     y = fullDeckList[a].Value[1];
-                    Console.WriteLine("{0} of {1} with values {2} and {3}", w, z, x, y);
-                } else if ( fullDeckList[a].Value.Length == 1 ) {
+                    Console.WriteLine ("{0} of {1} with values {2} and {3}", w, z, x, y);
+                } else if (fullDeckList[a].Value.Length == 1) {
                     x = fullDeckList[a].Value[0];
-                    Console.WriteLine("{0} of {1} with value {2}", w, z, x);
+                    Console.WriteLine ("{0} of {1} with value {2}", w, z, x);
                 }
             }
         }
--- CSharp/Practice/csDrawing1/Program.cs
+++ CSharp/Practice/csDrawing1/Program.cs
@@ -2,20 +2,17 @@
 using System.Drawing;
 using System.Windows.Forms;
 
-namespace csDrawing1
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Form f = new Form();
+namespace csDrawing1 {
+    class Program {
+        static void Main (string[] args) {
+            Form f = new Form ();
             f.BackColor = Color.White;
             f.FormBorderStyle = FormBorderStyle.None;
             f.Bounds = Screen.PrimaryScreen.Bounds;
             f.TopMost = true;
 
-            Application.EnableVisualStyles();
-            Application.Run(f);
+            Application.EnableVisualStyles ();
+            Application.Run (f);
         }
     }
 }
