cc43326fecd53c2e198d9a6d1aa271e6a7ebc30d
M B <m.blackistone@outlook.com>
2019-10-04 17:49:46 -0400

    update

---
+++ CSharp/Practice/ObjectPractice/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace ObjectPractice
+{
+    class Program
+    {
+        public class Lifeform
+        {
+            string Name { get; set; }
+            int Lifespan { get; set; }
+        }
+
+        public class Animal : Lifeform
+        {
+            bool Carnivore { get; set; }
+            bool Herbivore { get; set; }
+            bool Swims { get; set; }
+            bool Walks { get; set; }
+            bool Flies { get; set; }
+            bool Climbs { get; set; }
+        }
+        static GenerateMap(int width, int height, int numOceans)
+        {
+            //0 = Land
+            //1 = Water
+            //Initialize Map with all land
+            int[,] landMap = new int[width, height];
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    landMap(x, y) = 0;
+                }
+            }
+            //Seed Ocean Start Points
+            for (int a = 0; a < numOceans; a++)
+            {
+                Random landType = new Random();
+                int x = landType.Next(width);
+                int y = landType.Next(height);
+                landMap(x, y) = 1;
+            }
+            //Grow Oceans
+            for (a = 0; a < 1000; a++)
+            {
+                for (int y = 0; y < height; y++)
+                {
+                    for (int x = 0; x < width; x++)
+                    {
+                        int a = landType(4);
+                    }
+                }
+            }
+        }
+        //----------------------------------------------------
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
